53eec4a56f73a39dedcf08f42c7d1a8572e1f7e8
Alexey Matrosov <alexey.s.matrosov@gmail.com>
2024-07-10 12:06:57 +0300

    Add test inner page

---
+++ android_app/script.js
@@ -0,0 +1,22 @@
+document.getElementById('getCodeButton').addEventListener('click', function() {
+    // Получить текущий URL
+    const currentUrl = window.location.href;
+
+    window.open(currentUrl);
+
+    // // Получить хеш из URL
+    // const hash = window.location.hash;
+
+    // // Извлечь код из хеша
+    // const params = new URLSearchParams(hash.substring(1));
+    // const code = params.get('code');
+
+    // document.getElementById('codeDisplay').textContent = `URL: ${currentUrl}`;
+
+    // // Отобразить код
+    // if (code) {
+    //     document.getElementById('codeDisplay').textContent = `Code: ${currentUrl}`;
+    // } else {
+    //     document.getElementById('codeDisplay').textContent = 'Code not found';
+    // }
+});
